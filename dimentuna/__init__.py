faabb7113f0986dc8fe5f05fb5f3ef2c1080387b
ImalshaD <plid475@gmail.com>
2024-12-27 11:00:02 +0530

    Add LinearWrapper to __all__ exports and improve formatting

--- dimentuna/__init__.py
+++ dimentuna/__init__.py
@@ -2,6 +2,8 @@ from .Mappers import DTMapper, LinearMapper
 from .Models import DTHfEncoder, LayerWrappebleDTHfLLM, DTHfLLM, DTConfig
 from .Projectors import DTProjector, FeedForwardProjector, LSTMProjector, TransformerProjector
 from .Trainers import DTTrainStratergy, TwoPhasedTS, MixedTS
-from .Utils import DTLayerWrapper
+from .Utils import DTLayerWrapper, LinearWrapper
 
-__all__ = ['DTMapper', 'LinearMapper', 'DTHfEncoder', 'LayerWrappebleDTHfLLM', 'DTHfLLM', 'DTConfig', 'DTProjector', 'FeedForwardProjector', 'LSTMProjector', 'TransformerProjector', 'DTTrainStratergy', 'TwoPhasedTS', 'MixedTS', 'DTLayerWrapper']
+__all__ = ['DTMapper', 'LinearMapper', 'DTHfEncoder', 'LayerWrappebleDTHfLLM', 'DTHfLLM', 'DTConfig', 'DTProjector', 
+           'FeedForwardProjector', 'LSTMProjector', 'TransformerProjector', 'DTTrainStratergy', 'TwoPhasedTS', 'MixedTS',
+            'DTLayerWrapper', "LinearWrapper"]
