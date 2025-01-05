cd8ff8e4babd76b0d61ba5688f1ae31e38a7a9ef
ImalshaD <plid475@gmail.com>
2025-01-05 22:29:36 +0530

    Add LayerWrappebleQwen to models and update __all__ exports

--- dimentuna/__init__.py
+++ dimentuna/__init__.py
@@ -1,5 +1,5 @@
 from .Mappers import DTMapper, LinearMapper
-from .Models import DTHfEncoder, LayerWrappebleDTHfLLM, DTHfLLM, DTConfig
+from .Models import DTHfEncoder, LayerWrappebleDTHfLLM, DTHfLLM, DTConfig, LayerWrappebleQwen
 from .Projectors import DTProjector, FeedForwardProjector, LSTMProjector, TransformerProjector
 from .Trainers import DTTrainStratergy, TwoPhasedTS, MixedTS, TwoPhasedSeq2SeqTS
 from .Utils import DTLayerWrapper, LinearWrapper, NoiseWrapper
@@ -7,4 +7,4 @@ from .Datasets import DTDataset, DTMsgm
 __all__ = ['DTMapper', 'LinearMapper', 'DTHfEncoder', 'LayerWrappebleDTHfLLM', 'DTHfLLM', 'DTConfig', 'DTProjector', 
            'FeedForwardProjector', 'LSTMProjector', 'TransformerProjector', 'DTTrainStratergy', 'TwoPhasedTS', 'MixedTS',
             'DTLayerWrapper', "LinearWrapper", "NoiseWrapper", "TwoPhasedSeq2SeqTS",
-            'DTDataset', 'DTMsgm']
+            'DTDataset', 'DTMsgm', "LayerWrappebleQwen"]
